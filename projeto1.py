d702d828b0e797d6fb6f846fd4e86fd0a983254a
Daniel Francisco Magalhães de Castro <daniel.castro@fgv.br>
2024-08-13 12:00:17 -0300

    Adiciona função para sortear números e calcular a média

--- projeto1.py
+++ projeto1.py
@@ -1,3 +1,17 @@
 def cumprimento(nome):
  return f"Olá, {nome}! Seja bem-vinda!"
+
+print(cumprimento("Luana Pinheiro Pinto Silva")) 
+
+import random
+
+def cumprimento(nome):
+ return f"Olá, {nome}! Seja bem-vinda!"
+
+def sorteia_media():
+numeros = [random.randint(1, 50) for _ in range(3)] 
+media = sum(numeros) / len(numeros)
+return media
+
 print(cumprimento("Luana Pinheiro Pinto Silva"))
+print(f"A média dos números sorteados é: {sorteia_media()}")
